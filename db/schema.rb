d3a0d7305058583ac8e668d465b62700c94abcdd
Wataru-code <flatrockwalker@gmail.com>
2020-12-23 13:49:18 +0900

    ユーザー認証機能に必要なコントローラ・ビュー・モデルのファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_210204) do
+ActiveRecord::Schema.define(version: 2020_12_23_044055) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "idea_id"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2020_12_22_210204) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "comments", "ideas"
